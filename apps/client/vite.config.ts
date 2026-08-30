f82d1eeaaf2722da2a837b1b3eef1de7b58ce8c3
takuyadev <takuya.k.toyokawa@gmail.com>
2026-08-30 18:43:43 +0900

    fix: fix container not exposing port in Docker

--- apps/client/vite.config.ts
+++ apps/client/vite.config.ts
@@ -3,6 +3,11 @@ import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "vite";
 
 export default defineConfig({
+ 	server: {
+		host: "0.0.0.0",
+		strictPort: true
+  },
+	
   plugins: [tailwindcss(), reactRouter()],
   resolve: {
     tsconfigPaths: true,
